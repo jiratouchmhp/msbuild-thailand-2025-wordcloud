79e792c933a4c4f30fc8de564e13b86a344c4772
Isara Rungvitayakul <isaru_66@hotmail.com>
2025-05-28 19:48:38 +0700

    adjust unit test report

--- Server/jest.config.js
+++ Server/jest.config.js
@@ -3,5 +3,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
-  reporters: [['github-actions', {silent: false}], 'summary'],
+  reporters: [ "default", ["jest-junit", {"outputName": "jest-junit.xml"}] ]
 };
